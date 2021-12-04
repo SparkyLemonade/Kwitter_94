2650b0586f2057ab4d361d3240b8121f965dbeac
SparkyLemonade <86256663+SparkyLemonade@users.noreply.github.com>
2021-12-04 18:44:49 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+//Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_DPdjlxy_nC4cxajVk7SGjZGXSA02_zU",
+    authDomain: "kwitter-e4c0f.firebaseapp.com",
+    projectId: "kwitter-e4c0f",
+    storageBucket: "kwitter-e4c0f.appspot.com",
+    messagingSenderId: "269338075287",
+    appId: "1:269338075287:web:3faf738c4bdd863f8589d2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
